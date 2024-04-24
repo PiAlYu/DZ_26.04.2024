a1a04e38f93888811fc7b6eeda01e1c729fdffb6
aypir <aypirogov@gmail.com>
2024-04-24 15:49:36 +0300

    add files

--- 26/26.py
+++ 26/26.py
@@ -9,8 +9,7 @@ def find_index(lst, n):
             right = mid - 1
         else:
             left = mid + 1
-
-    return left if left < 10 ** 5 else left + 1
+    return left if left < 10 ** 5 else left
 
 f = open('26.txt').readlines()
 n, groups, classrooms = int(f[0]), list(map(int, f[1].split())), list(map(int, f[2].split()))
@@ -23,10 +22,10 @@ for i in range(n):
     if flag == 0:
         f = classrooms[i]
         ind = find_index(groups, f)
-        if ind == 100001:
+        if ind == n:
             flag = 1
         k *= ind - i
     else:
-        k *= 100001 - i
+        k *= n - i
 
 print(k%100000007, find_index(groups, classrooms[0]))
